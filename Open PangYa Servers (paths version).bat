22531a34db3db0b9da02f5f07eb5f0a1d0a55661
André <andremarinho21@outlook.com>
2018-10-14 14:34:44 -0300

    Add Open PangYa Servers (paths version).bat

---
+++ Open PangYa Servers (paths version).bat
@@ -0,0 +1,12 @@
+cd /d "B:\Games\PangYa Season 8\Server exes" 
+start "" "B:\Program Files\Wamp\wampmanager.exe"
+Press Enter to open server executable files...
+
+cd "B:\Games\PangYa Season 8\Server exes\Auth Server"
+start "" "B:\Games\PangYa Season 8\Server exes\Auth Server\Auth Server.exe"
+cd "B:\Games\PangYa Season 8\Server exes\Login Server"
+start "" "B:\Games\PangYa Season 8\Server exes\Login Server\LoginServer.exe"
+cd "B:\Games\PangYa Season 8\Server exes\Message Server"
+start "" "B:\Games\PangYa Season 8\Server exes\Message Server\Message Server.exe"
+cd "B:\Games\PangYa Season 8\Server exes\Game Server"
+start "" "B:\Games\PangYa Season 8\Server exes\Game Server\Game Server.exe"
